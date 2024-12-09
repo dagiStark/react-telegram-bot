eb5ccf0666ef434b7b05cb0d0e9760d9b7a4b1d3
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-12-09 11:33:41 +0300

    small fixes

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import Card from "./Card";
+import Cart from "./Cart";
+
+export { Card, Cart };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        custom: "1px -3px 52px -5px rgba(0, 0, 0, 0.77)", // Add your custom shadow
+      },
+    },
   },
   plugins: [],
-}
+};
