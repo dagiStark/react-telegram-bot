1731855d79b4bb1c85237a4768342b72751c7834
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-12-09 13:27:54 +0300

    initialized telegram miniApp connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import { Card, Cart } from "./components";
 import foods from "./constants/index";
 
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
+
+
 
 function App() {
   const [cartItems, setCartItems] = useState([]);
+  const telegramRef = useRef(null); // Initialize a ref
 
   function onAdd() {}
   function onRemove() {}
-  function handleCheckout() {}
+  function handleCheckout() {
+    telegramRef.MainButton.text = "Pay :";
+    telegramRef.MainButton.show();
+  }
+  useEffect(() => {
+    if (window.Telegram && window.Telegram.WebApp) {
+        telegramRef.current = window.Telegram.WebApp; // Store in ref
+        console.log(telegramRef.current);
+    } else {
+        console.error("Telegram WebApp is not initialized.");
+    }
+}, []);
   return (
     <>
       <div className="">
