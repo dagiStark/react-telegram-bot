2cfda99789a3b18dfa9c780282fda956389dc2b9
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-12-09 11:33:03 +0300

    dummy db data model

---
+++ src/constants/index.js
@@ -0,0 +1,61 @@
+import burger from "../assets/burger.png";
+import coca from "../assets/coca.png";
+import icecream from "../assets/icecream.png";
+import icecream1 from "../assets/icecream1.png";
+import kebab from "../assets/kebab.png";
+import pizza from "../assets/pizza.png";
+import salad from "../assets/salad.png";
+import water from "../assets/water.png";
+
+const foods = [
+  {
+    id: 1,
+    title: "Burger",
+    image: burger,
+    price: 5,
+  },
+  {
+    id: 2,
+    title: "Coca",
+    image: coca,
+    price: 2,
+  },
+  {
+    id: 3,
+    title: "Icecream",
+    image: icecream,
+    price: 3,
+  },
+  {
+    id: 4,
+    title: "Icecream1",
+    image: icecream1,
+    price: 3,
+  },
+  {
+    id: 5,
+    title: "Kebab",
+    image: kebab,
+    price: 4,
+  },
+  {
+    id: 6,
+    title: "Pizza",
+    image: pizza,
+    price: 6,
+  },
+  {
+    id: 7,
+    title: "Salad",
+    image: salad,
+    price: 3,
+  },
+  {
+    id: 8,
+    title: "Water",
+    image: water,
+    price: 1,
+  },
+];
+
+export default foods;
