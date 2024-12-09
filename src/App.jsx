cef8bd5684cdc265901ff76c9a058ae943af63c9
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-12-09 11:33:18 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { Card, Cart } from "./components";
+import foods from "./constants/index";
+
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
+
+  function onAdd() {}
+  function onRemove() {}
+  function handleCheckout() {}
   return (
     <>
-      <p>
-        Edit <code>src/App.jsx</code> and save to test HMR
-      </p>
+      <div className="">
+        <h1 className="text-center">Order Food</h1>
+        <Cart cartItems={cartItems} onCheckout={handleCheckout} />
+
+        <div className="flex flex-wrap justify-center items-center mt-12 ml-12">
+          {foods.map((food) => (
+            <Card key={food.id} food={food} onAdd={onAdd} onRemove={onRemove} />
+          ))}
+        </div>
+      </div>
     </>
   );
 }
