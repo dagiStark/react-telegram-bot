354fcf68968ec5e5b29c307f0c0d19c9f6523685
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-12-09 09:53:35 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p>
+        Edit <code>src/App.jsx</code> and save to test HMR
+      </p>
+    </>
+  );
+}
+
+export default App;
